01d61b208ee89514cb9b4b61e332e1ef238e8169
Vladimiro25 <uzunguk2000@gmail.com>
2025-08-03 17:57:46 +0300

    Lesson 7. Implementation of GameViewModel

--- app/src/main/java/com/example/composition/domain/entity/Question.kt
+++ app/src/main/java/com/example/composition/domain/entity/Question.kt
@@ -1,7 +1,10 @@
 package com.example.composition.domain.entity
 
 data class Question(
-    val sum:Int,
-    val visibleNumber:Int,
-    val option:List<Int>
-)
+    val sum: Int,
+    val visibleNumber: Int,
+    val option: List<Int>
+) {
+    val rightAnswer: Int
+        get() = sum - visibleNumber
+}
